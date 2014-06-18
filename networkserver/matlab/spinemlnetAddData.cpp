bfc7bfc68ce7f859b27bea39d24793009f5f1140
Seb James <seb.james@sheffield.ac.uk>
2014-06-18 10:41:29 +0100

    New mex function to add data to a connection (needs writing)

---
+++ networkserver/matlab/spinemlnetAddData.cpp
@@ -0,0 +1,91 @@
+/*
+ * Add data to a connection. Pass in both the name of the connection
+ * (this is the name it is given in the SpineCreator experiment
+ * interface) and the data to add to the connection.
+ *
+ * Returns a matrix containing: How much data is now in the
+ * connection; any error (such as "this connection is no an AM_TARGET
+ * connection" or "the named connection does not exist".
+ */
+
+#include "mex.h"
+#include <iostream>
+#include <map>
+#include <string.h>
+
+#include "SpineMLConnection.h"
+
+using namespace std;
+
+void
+mexFunction (int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    if (nrhs==0) {
+        mexErrMsgTxt("failed");
+    }
+
+    unsigned long long int *threadPtr; // Or some other type?
+    threadPtr = (unsigned long long int*)mxGetData(prhs[0]);
+    // This points to the main thread
+    pthread_t *thread = ((pthread_t*) threadPtr[0]);
+    // Has the main thread finished?
+    volatile bool *threadFinished = ((volatile bool*) threadPtr[2]);
+    // Active connections
+    map<pthread_t, SpineMLConnection*>* connections = (map<pthread_t, SpineMLConnection*>*) threadPtr[3];
+
+    bool tf = *threadFinished; // Seems to be necessary to make a copy
+                               // of the thing pointed to by
+                               // threadFinished.
+
+    // Get connection name from input arguments.
+
+    string errormsg("");
+
+    if (!tf) {
+
+        // Pass in some numbers from matlab - if a vector, then it's
+        // numbers for first connection. If >1 connection, it's numbers
+        // for several connections.
+
+        // sometype data = mxGetData(prhs[1]);
+
+        // Extract these, and add them, as possible, to the Connections,
+        // using name as index
+
+        // Debugging:
+        map<pthread_t, SpineMLConnection*>::iterator connIter = connections->begin();
+        while (connIter != connections->end()) {
+            cout << "Add data to a connection!" << endl;
+            // Do stuff here - add some data to the deque:
+            double d = 1.0;
+            connIter->second->addNum(d);
+            d += 1.0;
+            connIter->second->addNum(d);
+            d += 1.0;
+            connIter->second->addNum(d);
+            d += 1.0;
+            connIter->second->addNum(d);
+
+            ++connIter;
+        }
+
+    } else {
+        errormsg = "Can't add data; the main thread has finished.";
+    }
+
+    // The data for the output array. 2 numbers for now.
+    const mwSize res[2] = { 1, 2 }; // 1 by 2 matrix
+
+    // create the output array
+    // args: ndims, const mwSize *dims, mxClassID classid, complexity flag
+    //cout << "create numeric array..." << endl;
+    plhs[0] = mxCreateNumericArray (2, res, mxUINT16_CLASS, mxREAL);
+
+    // set up a pointer to the output array
+    unsigned short *outPtr = (unsigned short*) mxGetData(plhs[0]);
+
+    // copy new data into the output structure
+    unsigned short rtn[2] = { (unsigned short)*threadFinished, 0 };
+    //cout << "memcpy..." << endl;
+    memcpy(outPtr, (const void*)rtn, 4); // 2 bytes per short * 2 elements in rtn.
+}
