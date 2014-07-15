cb22352da8ea3d44e7c3219cc2dfd78351fac848
Seb James <seb.james@sheffield.ac.uk>
2014-07-15 14:32:35 +0100

    Some clarifications

--- networkserver/matlab/spinemlnet_run.m
+++ networkserver/matlab/spinemlnet_run.m
@@ -9,9 +9,10 @@ cleanupObj = onCleanup(@() spinemlnetCleanup(context));
 
 % First job - add some data for a connection
 
-% My sample experiment has 2 neurons in its single population and requires
-% 3000 points for each neuron. Here are two sines with 3000 points an
-% about 4 3/4 periods, one larger in amplitude than the other:
+% My sample experiment has 2 neurons in its single population and
+% requires 3000 points for each neuron. Here are two sines with 3000
+% points describing about 4 3/4 periods, one larger in amplitude than
+% the other:
 sine_array = 50 * sin([0:0.01:29.99]);
 sine_array2 = 10 * sin([0:0.01:29.99]);
 
@@ -21,15 +22,9 @@ sine_array2 = 10 * sin([0:0.01:29.99]);
 input_data = [sine_array;sine_array2];
 
 % Add the sine_array data to spinemlnet for the first connection,
-% which is called realtime.
+% which is called "realtime". This is the name that you give to the
+% connection when you create it in the SpineCreator "Experiment UI"
 [artn errormsg] = spinemlnetAddData (context, 'realtime', input_data);
-if length(errormsg) > 0
-    display (errormsg);
-end
-
-% Add the sine_array data to spinemlnet for a second connection,
-% which is called realtime2 (not used in my example lif_test2 expt).
-[artn errormsg] = spinemlnetAddData (context, 'realtime2', input_data);
 if length(errormsg) > 0
     display (errormsg);
 end
