61ec65f9ffa58a49b76c8fbc055627d4d9be4579
Seb James <seb.james@sheffield.ac.uk>
2015-08-20 17:14:55 +0100

    A convenience function for loading a neural sheet of the sort output by SpineCreator neural network populations

---
+++ analysis_utils/matlab/load_neural_sheet.m
@@ -0,0 +1,22 @@
+function data = load_neural_sheet (file_path)
+% Load double-formatted binary data
+
+    isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
+
+    % First, open the file:
+    [ fid, fopen_msg ] = fopen (file_path, 'r', 'native');
+    if fid == -1
+        display (['Failed to open file ', file_path, ' with error: ', ...
+                  fopen_msg]);
+        count = 0; data = [];
+        return;
+    end
+
+    data = [];
+    while ~feof(fid)
+        [ data1, cnt ] = fread (fid, [1], 'double=>double');
+        data = [data data1];
+    end
+
+    % Finally, would close the file here, but it seems to fail.
+end
