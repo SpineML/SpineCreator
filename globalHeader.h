6843751ecc7627f39c4120eb81125280aebc25f7
Seb James <seb.james@sheffield.ac.uk>
2020-09-08 11:27:33 +0100

    Adds constructors to struct loc

--- globalHeader.h
+++ globalHeader.h
@@ -152,6 +152,11 @@ public:
 };
 
 struct loc {
+    loc() : x(0.0f), y(0.0f), z(0.0f) {}
+    loc (float _x, float _y, float _z)
+        : x(_x)
+        , y(_y)
+        , z(_z) {}
     float x;
     float y;
     float z;
